48c4a0ac8104e22ddb1ca18c27d7c752301cab38
Admin <mihailbichkov@yandex.ru>
2020-07-10 10:47:19 +0500

    #202007101044

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -1,5 +1,13 @@
 package ru.job4j;
 
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class AppTest {
 
+    @Test
+    public void test() {
+        assertThat(1, is(1));
+    }
 }
